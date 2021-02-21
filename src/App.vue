798db0048f662ba40740dd9f4b9b9c16a7914adf
Giorgi Gvimradze <natceo@gmail.com>
2021-02-21 20:33:07 +0400

    last update

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <div class="container pt-5 ">
+            <h1 class="d-flex justify-content-center">{{hello}}</h1>
+
+            <form class="bg-light rounded p-3 " name="registration">
+                <div class="row justify-content-center">
+                    <div class="col-md">
+                        <div class="form-group col-sm">
+                            <label for="firstName">სახელი</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="შეიყვანიეთ სახელი" required>
+                            <label for="lastName">გვარი</label>
+                            <input type="text" class="form-control" id="lastName" placeholder="შეიყვანეთ გვარი" required>
+                            <label for="IDnumber">ID</label>
+                            <input type="number" class="form-control" id="IDnumber" placeholder="11 ციფრისგან შედგება" min="10000000000" max="99999999999" step="1" required>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group col-sm">
+                            <label for="gender" placeholder="asd">სქესი</label>
+                            <select class="form-control" id="gender" required>
+                                <option value="" disabled selected>კაცი/ქალი</option>
+                                <option>კაცი</option>
+                                <option>ქალი</option>
+                            </select>
+                            <label for="birthDate" class="mt-2">დაბადების თარიღი:</label>
+                            <input type="date" id="birthDate" name="birthDate" value="2000-01-01"> </br>
+                            <button type="button" class="btn btn-primary mt-2" onclick="insert('string')">რეგისტრაცია</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
---
+++ src/js/formValidation.js
@@ -0,0 +1,24 @@
+function validation(name,surname,ID,gender,date){
+    if (name === "" || /\d/.test(name)) {
+        alert("შეამოწმეთ სახელი");
+        return false;
+    }
+    else if(surname === "" || /\d/.test(surname)) {
+        alert("შეამოწმეთ გვარი");
+        return false;
+    }
+    else if(ID === "" || isNaN(ID)||ID<10000000000 || ID>99999999999) {
+        alert("შეამოწმეთ პირადი ნომერი");
+        return false;
+    }
+    else if(gender === "" || (gender!="კაცი" && gender!="ქალი")) {
+        console.log(gender)
+        alert("შეამოწმეთ სქესის ველი");
+        return false;
+    }
+    else if(date === "") {
+        alert("შეამოწმეთ დაბადების თარიღი");
+        return false;
+    }
+    return true
+}
